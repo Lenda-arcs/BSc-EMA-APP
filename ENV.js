15b9f6e60656dc3ea757ea836165418ee0a17485
Lenda-arcs <57134277+Lenda-arcs@users.noreply.github.com>
2021-04-06 16:07:41 +0200

    google api delete

--- ENV.js
+++ ENV.js
@@ -1,9 +1,6 @@
 const vars = {
-    // googleApiKey: 'AIzaSyDiWCxQT3nOg_cW3vQ_HkZRYwNFHkCOrxo',
-    // authGoogleApiKey: 'AIzaSyDMLa8ZvMv_VhnYwVppuCugwXZKNuXexNQ',
     TempOwnApi:'https://77f5a0642785.ngrok.io/api/v1',
     WeatherApi: 'https://api.synopticdata.com/v2',
-
     WeatherToken: '008ac7505ca54e30a8e973ed7da716d1',
 
 }
