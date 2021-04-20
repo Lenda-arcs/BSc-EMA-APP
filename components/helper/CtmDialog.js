b9d51141dce38cfc79e4377ed61984318e82f6fc
Lenda-arcs <57134277+Lenda-arcs@users.noreply.github.com>
2021-04-20 10:13:56 +0200

    Added scrollview to Dialog

--- components/helper/CtmDialog.js
+++ components/helper/CtmDialog.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
+import {ScrollView, View} from 'react-native';
 import { Button, Paragraph, Dialog, Portal } from 'react-native-paper';
 
 // Custom Dialog component that can be used to show details or explanation to user
@@ -11,12 +11,15 @@ const CtmDialog = (props) => {
             <Portal>
                 <Dialog visible={props.visible} onDismiss={props.hideDialog}>
                     <Dialog.Title>{props.title}</Dialog.Title>
-                    <Dialog.Content>
-                        <Paragraph>{props.helpText}</Paragraph>
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                        <Button onPress={props.hideDialog}>OKAY</Button>
-                    </Dialog.Actions>
+                    <ScrollView>
+                        <Dialog.Content>
+                            <Paragraph style={{textAlign: 'justify'}}>{props.helpText}</Paragraph>
+                        </Dialog.Content>
+                        <Dialog.Actions>
+                            <Button onPress={props.hideDialog}>OKAY</Button>
+                        </Dialog.Actions>
+                    </ScrollView>
+
                 </Dialog>
             </Portal>
         </View>
